22f99e841804e691b02d439525d12ba46c420055
Titanium <zukkafisch@gmail.com>
2026-04-05 19:37:43 -0600

    Refactor package name for MixinClientConnection

--- src/main/java/me/alpha432/oyvey/mixin/network/MixinClientConnection.java
+++ src/main/java/me/alpha432/oyvey/mixin/network/MixinClientConnection.java
@@ -1,6 +1,6 @@
-package reign.mixin;
+package com.oyvey.mixin.network;
 
-import reign.utils.PacketInterceptor;
+import com.oyvey.utils.PacketInterceptor;
 import net.minecraft.network.ClientConnection;
 import net.minecraft.network.packet.Packet;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientConnection.class)
 public class MixinClientConnection {
-    
     @Inject(method = "send(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"), cancellable = true)
     private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
         if (PacketInterceptor.onOutgoingPacket(packet)) {
