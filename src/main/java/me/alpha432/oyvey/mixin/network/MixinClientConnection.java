1f84879a67580df987bc481dec470686deb4187b
Titanium <zukkafisch@gmail.com>
2026-04-05 19:33:36 -0600

    Implement Mixin for ClientConnection to intercept packets

---
+++ src/main/java/me/alpha432/oyvey/mixin/network/MixinClientConnection.java
@@ -0,0 +1,20 @@
+package reign.mixin;
+
+import reign.utils.PacketInterceptor;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.packet.Packet;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientConnection.class)
+public class MixinClientConnection {
+    
+    @Inject(method = "send(Lnet/minecraft/network/packet/Packet;)V", at = @At("HEAD"), cancellable = true)
+    private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
+        if (PacketInterceptor.onOutgoingPacket(packet)) {
+            ci.cancel();
+        }
+    }
+}
