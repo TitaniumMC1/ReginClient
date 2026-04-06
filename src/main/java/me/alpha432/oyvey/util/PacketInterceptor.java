b796b347ec90e7bcabaf5ff95f1398893b8c5822
Titanium <zukkafisch@gmail.com>
2026-04-05 19:44:54 -0600

    Refactor PacketInterceptor to handle outgoing packets

--- src/main/java/me/alpha432/oyvey/util/PacketInterceptor.java
+++ src/main/java/me/alpha432/oyvey/util/PacketInterceptor.java
@@ -1,5 +1,6 @@
-package me.alpha432.oyvey.utils;
+package com.oyvey.utils;
 
+import net.minecraft.network.packet.Packet;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import java.util.function.Consumer;
@@ -17,8 +18,10 @@ public class PacketInterceptor {
     public static void onIncomingChunk(ChunkDataS2CPacket packet) {
         if (chunkHandler != null) chunkHandler.accept(packet);
     }
-    public static boolean onOutgoingMove(PlayerMoveC2SPacket packet) {
-        if (moveHandler != null) moveHandler.accept(packet);
-        return false;
+    public static boolean onOutgoingPacket(Packet<?> packet) {
+        if (packet instanceof PlayerMoveC2SPacket && moveHandler != null) {
+            moveHandler.accept((PlayerMoveC2SPacket) packet);
+        }
+        return false; // never cancel by default
     }
 }
